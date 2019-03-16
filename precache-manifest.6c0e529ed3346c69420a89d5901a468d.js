3d362a447e4fb01b1c6de9e2ca3ceae654866097
Amir Bizimana <amirbzmn@gmail.com>
2019-03-16 01:52:53 -0400

    Updates

---
+++ precache-manifest.6c0e529ed3346c69420a89d5901a468d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4075abf83183435df88",
+    "url": "/kid-studio-react/static/js/runtime~main.f4075abf.js"
+  },
+  {
+    "revision": "22d980de721bd68f23b0",
+    "url": "/kid-studio-react/static/js/main.22d980de.chunk.js"
+  },
+  {
+    "revision": "a3aa1e297414edbbb048",
+    "url": "/kid-studio-react/static/js/2.a3aa1e29.chunk.js"
+  },
+  {
+    "revision": "22d980de721bd68f23b0",
+    "url": "/kid-studio-react/static/css/main.507844fa.chunk.css"
+  },
+  {
+    "revision": "b74122d671aad649c78393dcf828b859",
+    "url": "/kid-studio-react/index.html"
+  }
+];
