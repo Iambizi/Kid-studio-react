b53f6486cde867062eff1446bfdbf02514f085cd
Amir Bizimana <amirbzmn@gmail.com>
2019-03-20 01:57:02 -0400

    film component refactoring

--- src/components/film.jsx
+++ src/components/film.jsx
@@ -13,7 +13,60 @@ const backgroundPosition = {
 };
 
 class Film extends Component {
-  state = { title: "Film" };
+  state = {
+    title: "Film",
+    filmName: [
+      "Reel 2018",
+      "Nike React Hyperdunk",
+      "French Montana ft. Drake 'No Stylist'",
+      "The Weeknd 'Reminder'",
+      "The Weeknd 'King of The Fall'",
+      "Big Sean 'Bounce Back'",
+      "Future 'Comin Out Strong'",
+      "Hard to Kill 'Slime'",
+      "Nowness Short: Cyber69",
+      "Red Wine Revelations 119",
+      "6LACK 'Ex Calling'",
+      "Saya 'Wet Dreams'",
+      "Roy Woods 'Go Go Go'",
+      "Roy Woods 'Gwan Big Up Urself'",
+      "Majid Jordan 'Small Talk'"
+    ],
+    filmLink: [
+      "http://kidstudio.co/film/reel-2018",
+      "http://kidstudio.co/film/nikereact",
+      "http://kidstudio.co/film/no-stylist",
+      "http://kidstudio.co/film/reminder",
+      "http://kidstudio.co/film/king-of-the-fall",
+      "http://kidstudio.co/film/bounce-back",
+      "http://kidstudio.co/film/cos",
+      "http://kidstudio.co/film/slime",
+      "http://kidstudio.co/film/cyber69",
+      "http://kidstudio.co/film/rwr119",
+      "http://kidstudio.co/film/ex-calling",
+      "http://kidstudio.co/film/wet-d",
+      "http://kidstudio.co/film/go-go-g",
+      "http://kidstudio.co/film/gbuu",
+      "http://kidstudio.co/film/smalltalk"
+    ],
+    hoverGif: [
+      "http://kidstudio.co/film/reel-2018/reel.gif",
+      "http://kidstudio.co/film/nikereact/nike.gif",
+      "http://kidstudio.co/film/no-stylist/stylist.gif",
+      "http://kidstudio.co/film/reminder/reminder.gif",
+      "http://kidstudio.co/film/king-of-the-fall/featured1.png",
+      "http://kidstudio.co/film/bounce-back/bb.gif",
+      "http://kidstudio.co/film/cos/cos.gif",
+      "http://kidstudio.co/film/slime/htk.gif",
+      "http://kidstudio.co/film/cyber69/cyber.gif",
+      "http://kidstudio.co/film/rwr119/rwr-bmp.gif",
+      "http://kidstudio.co/film/ex-calling/excalling.gif",
+      "http://kidstudio.co/film/wet-d/wetdreams.gif",
+      "http://kidstudio.co/film/go-go-g/go.gif",
+      "http://kidstudio.co/film/gbuu/gbuu.gif",
+      "http://kidstudio.co/film/smalltalk/smalltalk.gif"
+    ]
+  };
 
   constructor() {
     super();
@@ -29,6 +82,9 @@ class Film extends Component {
     //   el: ".projects"
     //});
   };
+  componentDidMount = () => {
+    console.log();
+  };
   render() {
     return (
       <React.Fragment>
@@ -38,7 +94,16 @@ class Film extends Component {
             <div className="page-headers">{this.state.title}</div>
             <div className="project-list">
               <center>
-                <Link
+                {this.state.filmName.map((name, i) => (
+                  <React.Fragment key={i}>
+                    <a className="project-links" href={this.state.filmLink[i]}>
+                      {this.state.filmName[i]}
+                    </a>
+                    <br />
+                  </React.Fragment>
+                ))}
+
+                {/* <Link
                   onMouseOver={this.okHova}
                   ref={this.link}
                   data-okimage="http://kidstudio.co/film/reel-2018/reel.gif"
@@ -159,7 +224,7 @@ class Film extends Component {
                 >
                   Majid Jordan 'Small Talk'
                 </Link>
-                <br />
+                <br /> */}
               </center>
             </div>
           </div>
